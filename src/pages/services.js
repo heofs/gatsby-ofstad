1a5e78ebf11e79f55a545295f6fb91e0cf29353b
Henning Ofstad <ofstad10@gmail.com>
2018-11-06 13:37:15 +0000

    add product website container

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
+// import Img from 'gatsby-image'
+import WebsiteImage from '../images/svg/browser.svg'
 
 import Layout from '../layouts/layout'
 
 const ServicesPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div
+      className="container-fluid product-container product-bg-grey justify-content-center px-0"
+      id="websites"
+    >
+      <div className="row col-xs-12 col-xl-10 product-row py-5 mx-auto justify-content-center">
+        <div className="col-xs-12 col-md- text-center product-image-container product-col">
+          <img
+            className="product-main-image"
+            style={{ height: '12rem' }}
+            src={WebsiteImage}
+            alt="website illustration"
+          />
+        </div>
+        <div className="col-xs-12 col-md-8 px-0 px-md-4 product-col align-self-start">
+          <div className="product-text">
+            <h1>Websites</h1>
+            <p>
+              Responsive websites for your business or project that scales well
+              with any device.
+            </p>
+            <p>Optimized for search engine ranking and speed.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
