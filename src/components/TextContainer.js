4e5e3d4e055dbf802efb6cad27097ee6507a25c8
Henning Ofstad <ofstad10@gmail.com>
2018-11-09 16:58:54 +0000

    set maxwidth for text

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -2,17 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const TextContainer = ({ title, children, greyBG }) => {
+  const maxWidth = '800px'
   return (
     <div
-      className="container-fluid border-bottom py-4"
+      className="container-fluid border-bottom p-4"
       style={
         greyBG === true
           ? { backgroundColor: '#f6f6f6' }
           : { backgroundColor: '#fff' }
       }
     >
-      <h2 className="container-text-heading">{title}</h2>
-      <p className="m-0">{children}</p>
+      <div
+        className="row justify-content-center mx-auto px-1"
+        style={{
+          maxWidth: maxWidth,
+        }}
+      >
+        <h2 className="container-text-heading"> {title} </h2>
+      </div>
+      <div
+        className="row justify-content-center mx-auto px-1"
+        style={{
+          maxWidth: maxWidth,
+        }}
+      >
+        <p className="m-0" style={{ textAlign: 'center' }}>
+          {children}
+        </p>
+      </div>
     </div>
   )
 }
